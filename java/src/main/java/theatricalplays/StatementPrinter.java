07cc5cdfdae54b1002e1b797227c1ba950acf28e
Paolo Sciarra <itbhps@gmail.com>
2025-02-13 19:56:22 +0100

    removed duplicated for and used stream and cumulative Triple object

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -1,7 +1,6 @@
 package theatricalplays;
 
 import java.text.NumberFormat;
-import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Map;
 
@@ -16,46 +15,37 @@ public class StatementPrinter {
   }
 
   public String print(Invoice invoice) {
-    var result = String.format("Statement for %s\n", invoice.customer());
-    var playAmounts = new LinkedList<Integer>();
-    var playVolumeCredits = new LinkedList<Integer>();
+    var playsInfo =
+        invoice.performances().stream()
+            .map(
+                performance -> {
+                  var play = retrievePlayBy(performance.playId());
+                  var playAmount = playAmount(performance, play);
+                  var volumeCredit = volumeCredit(performance, play);
+                  var message =
+                      String.format(
+                          "  %s: %s (%s seats)",
+                          play.name(),
+                          numberFormat.format(playAmount / 100),
+                          performance.audience());
+                  return new Triple(message, playAmount, volumeCredit);
+                })
+            .reduce(new Triple("", 0, 0), Triple::combine);
 
-    for (var performance : invoice.performances()) {
-      var play = retrievePlayBy(performance.playId());
-      var volumeCredit = Math.max(performance.audience() - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy".equals(play.type())) volumeCredit += Math.floor(performance.audience() / 5);
-      playVolumeCredits.add(volumeCredit);
+    var totalAmounts = playsInfo.playAmount;
+    var volumeCredits = playsInfo.volumeCredit;
+    return String.format("Statement for %s", invoice.customer())
+        + playsInfo.message + "\n"
+        + String.format("Amount owed is %s\n", numberFormat.format(totalAmounts / 100))
+        + String.format("You earned %s credits\n", volumeCredits);
+  }
+
+  private int volumeCredit(Performance performance, Play play) {
+    var volumeCredit = Math.max(performance.audience() - 30, 0);
+    if ("comedy".equals(play.type())) {
+      return volumeCredit + Double.valueOf((double) performance.audience() / 5).intValue();
     }
-
-    for (var performance : invoice.performances()) {
-      var play = retrievePlayBy(performance.playId());
-      var thisAmount = playAmount(performance, play);
-      playAmounts.add(thisAmount);
-    }
-
-    for (var performance : invoice.performances()) {
-      var play = retrievePlayBy(performance.playId());
-      var thisAmount = playAmount(performance, play);
-      // print line for this order
-      result +=
-          String.format(
-              "  %s: %s (%s seats)\n",
-              play.name(), numberFormat.format(thisAmount / 100), performance.audience());
-    }
-
-    invoice.performances().stream()
-        .map(
-            performance -> {
-              var play = retrievePlayBy(performance.playId());
-              return new Pair(performance, play);
-            });
-
-    var totalAmounts = playAmounts.stream().mapToInt(Integer::intValue).sum();
-    var volumeCredits = playVolumeCredits.stream().mapToInt(Integer::intValue).sum();
-    result += String.format("Amount owed is %s\n", numberFormat.format(totalAmounts / 100));
-    result += String.format("You earned %s credits\n", volumeCredits);
-    return result;
+    return volumeCredit;
   }
 
   private int playAmount(Performance performance, Play play) {
@@ -85,7 +75,12 @@ public class StatementPrinter {
     return this.playsIdToPlay.get(playId);
   }
 
-  private record Pair<A, B>(A a, B b) {}
-
-  private record Triple<A, B, C>(A a, B b, C c) {}
+  private record Triple(String message, Integer playAmount, Integer volumeCredit) {
+    public Triple combine(Triple triple) {
+      return new Triple(
+          this.message + "\n" + triple.message,
+          this.playAmount + triple.playAmount,
+          this.volumeCredit + triple.volumeCredit);
+    }
+  }
 }
