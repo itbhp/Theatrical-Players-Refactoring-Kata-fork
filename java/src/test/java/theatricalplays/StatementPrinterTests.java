74c3ddb23c727da0e0b0627a8ec18b236ffca088
Paolo Sciarra <itbhps@gmail.com>
2025-02-13 18:42:05 +0100

    minor java changes

--- java/src/test/java/theatricalplays/StatementPrinterTests.java
+++ java/src/test/java/theatricalplays/StatementPrinterTests.java
@@ -1,28 +1,28 @@
 package theatricalplays;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Map;
 
 import static org.approvaltests.Approvals.verify;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class StatementPrinterTests {
+class StatementPrinterTests {
 
     @Test
     void exampleStatement() {
-        Map<String, Play> plays = Map.of(
+        var plays = Map.of(
                 "hamlet",  new Play("Hamlet", "tragedy"),
                 "as-like", new Play("As You Like It", "comedy"),
                 "othello", new Play("Othello", "tragedy"));
 
-        Invoice invoice = new Invoice("BigCo", List.of(
+        var invoice = new Invoice("BigCo", List.of(
                 new Performance("hamlet", 55),
                 new Performance("as-like", 35),
                 new Performance("othello", 40)));
 
-        StatementPrinter statementPrinter = new StatementPrinter();
+        var statementPrinter = new StatementPrinter();
         var result = statementPrinter.print(invoice, plays);
 
         verify(result);
@@ -30,17 +30,15 @@ public class StatementPrinterTests {
 
     @Test
     void statementWithNewPlayTypes() {
-        Map<String, Play> plays = Map.of(
+        var plays = Map.of(
                 "henry-v",  new Play("Henry V", "history"),
                 "as-like", new Play("As You Like It", "pastoral"));
 
-        Invoice invoice = new Invoice("BigCo", List.of(
+        var invoice = new Invoice("BigCo", List.of(
                 new Performance("henry-v", 53),
                 new Performance("as-like", 55)));
 
-        StatementPrinter statementPrinter = new StatementPrinter();
-        Assertions.assertThrows(Error.class, () -> {
-            statementPrinter.print(invoice, plays);
-        });
+        var statementPrinter = new StatementPrinter();
+        assertThrows(Error.class, () -> statementPrinter.print(invoice, plays));
     }
 }
