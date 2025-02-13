2605dca045ea6cc60b3486be342c81f252e6a9ed
Paolo Sciarra <itbhps@gmail.com>
2025-02-13 19:33:48 +0100

    added google java format plugin

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -7,71 +7,85 @@ import java.util.Map;
 
 public class StatementPrinter {
 
-    private final NumberFormat numberFormat;
-    private final Map<PlayId, Play> playsIdToPlay;
+  private final NumberFormat numberFormat;
+  private final Map<PlayId, Play> playsIdToPlay;
 
-    public StatementPrinter(Map<PlayId, Play> playsIdToPlay) {
-        this.playsIdToPlay = playsIdToPlay;
-        numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
+  public StatementPrinter(Map<PlayId, Play> playsIdToPlay) {
+    this.playsIdToPlay = playsIdToPlay;
+    numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
+  }
+
+  public String print(Invoice invoice) {
+    var result = String.format("Statement for %s\n", invoice.customer());
+    var playAmounts = new LinkedList<Integer>();
+    var playVolumeCredits = new LinkedList<Integer>();
+
+    for (var performance : invoice.performances()) {
+      var play = retrievePlayBy(performance.playId());
+      var volumeCredit = Math.max(performance.audience() - 30, 0);
+      // add extra credit for every ten comedy attendees
+      if ("comedy".equals(play.type())) volumeCredit += Math.floor(performance.audience() / 5);
+      playVolumeCredits.add(volumeCredit);
     }
 
-    public String print(Invoice invoice) {
-        var result = String.format("Statement for %s\n", invoice.customer());
-        var playAmounts = new LinkedList<Integer>();
-        var playVolumeCredits = new LinkedList<Integer>();
-
-        for (var performance : invoice.performances()) {
-            var play = retrievePlayBy(performance.playId());
-            var volumeCredit = Math.max(performance.audience() - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy".equals(play.type())) volumeCredit += Math.floor(performance.audience() / 5);
-            playVolumeCredits.add(volumeCredit);
-        }
-
-        for (var performance : invoice.performances()) {
-            var play = retrievePlayBy(performance.playId());
-            var thisAmount = playAmount(performance, play);
-            playAmounts.add(thisAmount);
-        }
-
-        for (var performance : invoice.performances()) {
-            var play = retrievePlayBy(performance.playId());
-            var thisAmount = playAmount(performance, play);
-            // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name(), numberFormat.format(thisAmount / 100), performance.audience());
-        }
-
-        var totalAmounts = playAmounts.stream().mapToInt(Integer::intValue).sum();
-        var volumeCredits = playVolumeCredits.stream().mapToInt(Integer::intValue).sum();
-        result += String.format("Amount owed is %s\n", numberFormat.format(totalAmounts / 100));
-        result += String.format("You earned %s credits\n", volumeCredits);
-        return result;
+    for (var performance : invoice.performances()) {
+      var play = retrievePlayBy(performance.playId());
+      var thisAmount = playAmount(performance, play);
+      playAmounts.add(thisAmount);
     }
 
-    private int playAmount(Performance performance, Play play) {
-        var thisAmount = 0;
+    for (var performance : invoice.performances()) {
+      var play = retrievePlayBy(performance.playId());
+      var thisAmount = playAmount(performance, play);
+      // print line for this order
+      result +=
+          String.format(
+              "  %s: %s (%s seats)\n",
+              play.name(), numberFormat.format(thisAmount / 100), performance.audience());
+    }
 
-        switch (play.type()) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (performance.audience() > 30) {
-                    thisAmount += 1000 * (performance.audience() - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (performance.audience() > 20) {
-                    thisAmount += 10000 + 500 * (performance.audience() - 20);
-                }
-                thisAmount += 300 * performance.audience();
-                break;
-            default:
-                throw new Error("unknown type: ${play.type}");
+    invoice.performances().stream()
+        .map(
+            performance -> {
+              var play = retrievePlayBy(performance.playId());
+              return new Pair(performance, play);
+            });
+
+    var totalAmounts = playAmounts.stream().mapToInt(Integer::intValue).sum();
+    var volumeCredits = playVolumeCredits.stream().mapToInt(Integer::intValue).sum();
+    result += String.format("Amount owed is %s\n", numberFormat.format(totalAmounts / 100));
+    result += String.format("You earned %s credits\n", volumeCredits);
+    return result;
+  }
+
+  private int playAmount(Performance performance, Play play) {
+    var thisAmount = 0;
+
+    switch (play.type()) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (performance.audience() > 30) {
+          thisAmount += 1000 * (performance.audience() - 30);
         }
-        return thisAmount;
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (performance.audience() > 20) {
+          thisAmount += 10000 + 500 * (performance.audience() - 20);
+        }
+        thisAmount += 300 * performance.audience();
+        break;
+      default:
+        throw new Error("unknown type: ${play.type}");
     }
+    return thisAmount;
+  }
 
-    private Play retrievePlayBy(PlayId playId) {
-        return this.playsIdToPlay.get(playId);
-    }
+  private Play retrievePlayBy(PlayId playId) {
+    return this.playsIdToPlay.get(playId);
+  }
+
+  private record Pair<A, B>(A a, B b) {}
+
+  private record Triple<A, B, C>(A a, B b, C c) {}
 }
--- java/src/test/java/theatricalplays/StatementPrinterTests.java
+++ java/src/test/java/theatricalplays/StatementPrinterTests.java
@@ -10,50 +10,46 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StatementPrinterTests {
 
-    @Test
-    void exampleStatement() {
+  @Test
+  void exampleStatement() {
 
-        var hamletId = new PlayId("hamlet");
-        var asLikeId = new PlayId("as-like");
-        var othelloId = new PlayId("othello");
+    var hamletId = new PlayId("hamlet");
+    var asLikeId = new PlayId("as-like");
+    var othelloId = new PlayId("othello");
 
-        var playsIdToPlay = Map.of(
-                hamletId, new Play("Hamlet", "tragedy"),
-                asLikeId, new Play("As You Like It", "comedy"),
-                othelloId, new Play("Othello", "tragedy"));
+    var playsIdToPlay =
+        Map.of(
+            hamletId, new Play("Hamlet", "tragedy"),
+            asLikeId, new Play("As You Like It", "comedy"),
+            othelloId, new Play("Othello", "tragedy"));
 
-        var invoice = new Invoice(
-                "BigCo",
-                List.of(
-                        new Performance(hamletId, 55),
-                        new Performance(asLikeId, 35),
-                        new Performance(othelloId, 40)
-                )
-        );
+    var invoice =
+        new Invoice(
+            "BigCo",
+            List.of(
+                new Performance(hamletId, 55),
+                new Performance(asLikeId, 35),
+                new Performance(othelloId, 40)));
 
-        var statementPrinter = new StatementPrinter(playsIdToPlay);
-        var result = statementPrinter.print(invoice);
+    var statementPrinter = new StatementPrinter(playsIdToPlay);
+    var result = statementPrinter.print(invoice);
 
-        verify(result);
-    }
+    verify(result);
+  }
 
-    @Test
-    void statementWithNewPlayTypes() {
+  @Test
+  void statementWithNewPlayTypes() {
 
-        var henryVId = new PlayId("henry-v");
-        var asLikeId = new PlayId("as-like");
-        var invoice = new Invoice(
-                "BigCo",
-                List.of(
-                        new Performance(henryVId, 53),
-                        new Performance(asLikeId, 55)
-                )
-        );
-        var playsIdToPlay = Map.of(
-                henryVId, new Play("Henry V", "history"),
-                asLikeId, new Play("As You Like It", "pastoral"));
+    var henryVId = new PlayId("henry-v");
+    var asLikeId = new PlayId("as-like");
+    var invoice =
+        new Invoice("BigCo", List.of(new Performance(henryVId, 53), new Performance(asLikeId, 55)));
+    var playsIdToPlay =
+        Map.of(
+            henryVId, new Play("Henry V", "history"),
+            asLikeId, new Play("As You Like It", "pastoral"));
 
-        var statementPrinter = new StatementPrinter(playsIdToPlay);
-        assertThrows(Error.class, () -> statementPrinter.print(invoice));
-    }
+    var statementPrinter = new StatementPrinter(playsIdToPlay);
+    assertThrows(Error.class, () -> statementPrinter.print(invoice));
+  }
 }
