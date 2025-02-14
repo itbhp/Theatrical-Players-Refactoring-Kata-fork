80ae0809189192a8ab98c215de23ab5175a34934
Paolo Sciarra <itbhps@gmail.com>
2025-02-14 11:37:17 +0100

    reformat

--- java/src/main/java/theatricalplays/GenericPlay.java
+++ java/src/main/java/theatricalplays/GenericPlay.java
@@ -1,8 +1,8 @@
 package theatricalplays;
 
-public record GenericPlay(String name, String type) implements Play{
-    @Override
-    public int amountFor(Audience audience) {
-        throw new Error("unknown type: ${play.type}");
-    }
+public record GenericPlay(String name, String type) implements Play {
+  @Override
+  public int amountFor(Audience audience) {
+    throw new Error("unknown type: ${play.type}");
+  }
 }
